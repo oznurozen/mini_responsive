ec1df9822b3474a360064d6ceb658a22b6f964fc
Habip ELİŞ <habibelis65@gmail.com>
2022-07-03 15:17:56 +0300

    #hatasız değer alma

---
+++ pytg.py
@@ -0,0 +1,12 @@
+print("************işlem**************")
+while True:
+    try:
+        secenek = int(input("Şecenek = "))
+        a = int(input("sayı1 = "))
+        b = int(input("sayı2 = "))
+        if secenek =="+":
+            pass
+
+    except:
+        print("(+ , - , X , /, bitir) kullanabilirsiniz.")
+    
